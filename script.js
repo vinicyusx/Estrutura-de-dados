6442d27904309514196522318aa12eabdc9143e1
vinicyusx <vinicyus3029@gmail.com>
2022-01-25 17:34:29 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let name = "Vinicyus"
+let number = 7
+let year = 2022
+let result = year - number
+
+ console.log(result)
+
+ console.log("Estou mexendo no console.log")
+
+ let QuartaFeira = Boolean(false)
+ 
+ console.log(QuartaFeira)
+
+ console.log(typeof QuartaFeira)
+
+
